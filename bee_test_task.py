1eae92789de7191f4657afc7a188c9b79de27b9d
zhibekaa <zhibekme@gmail.com>
2019-05-30 18:11:56 +0600

    Update bee_test_task.py

--- bee_test_task.py
+++ bee_test_task.py
@@ -2,9 +2,9 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 with urllib.request.urlopen("https://www.nur.kz/") as url:
-    s = url.read().decode("utf-8")
+    page = url.read().decode("utf-8")
 
-soup = BeautifulSoup(s, "lxml")
+soup = BeautifulSoup(page, "lxml")
 
 t2 = soup.findAll('title')
 print(t2)
